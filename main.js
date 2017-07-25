0d713a09a4d1b2e4d678201e00e298350f339d6d
Imani Oliver <imani.oliver@gmail.com>
2017-07-25 15:55:23 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,242 @@
+// Answer the following questions in this file using comments without running the code
+
+// 1.
+// What does `givenName` equal right now?
+let givenName;
+if (givenName){
+  console.log (givenNameundefined)}
+
+// A: undefined
+
+
+// 2.
+// What is `givenName` set to right now?
+givenName = "Tim";
+// A: Tim
+
+
+// 3.
+// What is `givenName` set to right now?
+givenName = givenName;
+// A: Tim
+
+
+// 4.
+// What is `greeting` set to?
+let greeting = "Hello, how are you? " + givenName;
+// A: "Hello, how are you? Tim"
+
+
+// 5.
+// What is `math` set to?
+let high = 50;
+let low  = 10;
+let math = high - low;
+// A: 40
+
+
+// 6.
+// What is `math` set to?
+math = high - "5";
+// A: undefined
+
+
+// 7.
+// Create a variable to calculate Tim's age
+// The answer should read "Tim is 33 years old"
+// The answer shoud not be written in a comment.
+let born = 1984;
+let today = 2017;
+// A:
+let TimAge = today - born;
+
+
+let answer = givenName + "is" + TimAge + "years old.";
+
+// 8.
+// Adjust this code. Store some information in the following variables.
+// A:
+let yourName = "Imani";
+let instructorName = "Isaac";
+
+
+// 9.
+// Update the variables `yourName` and `instructorName` so the expression below reads correctly.
+// A:
+
+
+// This statement should read correctly
+let statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
+
+
+// 10.
+// We've created a variable named after each primitive type.
+// Use the `=` operator to assign a value to each variable matching the type in the name.
+let myNumber = 3476139121;
+let myString = "blahdeeblah";
+let myBoolean = false;
+// This one's a little tricky - think carefully about what makes a value "undefined"!
+let myUndefined = "";
+// A: ^above
+
+
+// For questions 11 - 19: List which boolean value will be stored in each variable. (Remember: `==` does not check data type)
+
+// 11.
+let x = (false == 0);
+if (false == 0) {
+  console.log ("truehomie1!")}
+// A. true
+
+
+// 12.
+let y = (false == "");
+// A. true
+
+
+// 13.
+let z = (0 == "");
+if (0 == "") {
+  console.log ("truehomie3!")}
+// A. true *****
+
+
+// 14.
+let a = (null == null);
+// A. true
+
+
+// 25.
+let b = (undefined == undefined);
+if (undefined ==undefined) {
+  console.log ("truehomie!")
+}
+// A. true
+
+
+// 16.
+let c = (undefined == null);
+// A. false
+
+
+// 17.
+let d = (null == false);
+// A. true
+
+
+// 18.
+let e = (NaN == null);
+// A. true
+
+
+// 19.
+let f = (NaN == NaN);
+if (NaN == NaN) {
+  console.log ("truehomie45!")}
+  else {
+      console.log ("nahhomie!")
+  }
+// A.false
+//why?
+
+
+// 20.
+// Fix the `if...else` statement below by adding an `if` keyword and conditional expression.
+// Use the variable `thirsty` as part of your conditional expression.
+// A:
+let thirsty = true;
+if (thirsty === true) {
+  console.log("I'm parched!");
+} else { (thirsty === false)
+  console.log("I've had enough to drink.");
+}
+
+// For questions 21 - 29 list which statement will be logged.
+
+// 21.
+let xy;
+
+if(xy){
+	console.log("True!");
+} else {
+	console.log("False!");
+}
+// A:false- it is undefined
+
+
+// 22.
+if(""){ //undefined variable
+	console.log("True!");
+} else {
+	console.log("False!");
+}
+// A: false
+
+
+// 23.
+// this expression will set x to NaN
+x = 1 * undefined;
+
+if(x){
+	console.log("True!");
+} else {
+	console.log("False!");
+}
+// A: false
+
+
+// 24.
+if(false){
+	console.log("True!");
+} else {
+	console.log("False!");
+}
+// A: false
+
+
+// 25.
+if(-5 && 5){
+	console.log("True");
+} else {
+	console.log("False!");
+}
+// A:true
+
+
+// 26.
+if("Hello World!"){
+	console.log("True!");
+} else {
+	console.log("False!");
+}
+// A: TRUE
+
+
+// 27.
+if({}){
+	console.log("True!");
+} else {
+	console.log("False!");
+}
+// A:true, because this is an object, and this is telling me that an object is here with {}
+
+// 28.
+if({name: "Yehuda Katz"}){
+	console.log("True!");
+} else {
+	console.log("False!");
+}
+// A:true; objects is also here
+
+
+// 29.
+if(new Date()){
+	console.log("True!");
+} else {
+	console.log("False!");
+}
+// A:true the Date is undefined byt the new is there...?
+
+
+// 30.
+// Link this file to the sibling `index.html` document, open the file in your browser and make sure there are no errors in your console.
